0103ae20f95bc0ef7deba30657153396639a6b71
xfeusw <khamrakulovkamron@gmail.com>
2025-12-27 05:00:03 +0500

    chore: completed exercise 11 from set2

--- exercises/Set2a.hs
+++ exercises/Set2a.hs
@@ -175,4 +175,6 @@ eitherDiv x y = Right $ x `div` y
 --   addEithers (Left "boom") (Left "fail") ==> Left "boom"
 
 addEithers :: Either String Int -> Either String Int -> Either String Int
-addEithers a b = todo
+addEithers (Right x) (Right y) = Right (x + y)
+addEithers (Left s) _ = Left s
+addEithers _ (Left s) = Left s
