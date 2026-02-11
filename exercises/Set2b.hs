93650d0f5999cc80f1c7d55f2b87e4d74d4c9846
xfeusw <khamrakulovkamron@gmail.com>
2026-02-11 16:47:44 +0500

    chore: second exercise from set2b is completed

--- exercises/Set2b.hs
+++ exercises/Set2b.hs
@@ -16,7 +16,11 @@ import Data.List
 -- Hint! pattern matching is your friend.
 
 binomial :: Integer -> Integer -> Integer
-binomial = todo
+binomial n k
+  | k == 0    = 1
+  | k == n    = 1
+  | k > n     = 0
+  | otherwise = binomial (n - 1) k + binomial (n - 1) (k - 1)
 
 ------------------------------------------------------------------------------
 -- Ex 2: implement the odd factorial function. Odd factorial is like
@@ -27,7 +31,10 @@ binomial = todo
 --   oddFactorial 6 ==> 5*3*1 ==> 15
 
 oddFactorial :: Integer -> Integer
-oddFactorial = todo
+oddFactorial n
+  | n <= 1 = 1
+  | even n = oddFactorial (n - 1)
+  | otherwise = n * (oddFactorial (n - 2))
 
 ------------------------------------------------------------------------------
 -- Ex 3: implement the Euclidean Algorithm for finding the greatest
